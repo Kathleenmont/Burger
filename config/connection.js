96b64bb99e14118d0903af32dbaad5e707f41918
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-05-13 11:54:47 -0400

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 var mysql = require('mysql')
+var connection;
 
-// mysql setup 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "yourRootPassword",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // mysql setup 
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "yourRootPassword",
+        database: "burgers_db"
+    });
+}
 
 // mysql connection
-connection.connect( function (err) {
+connection.connect(function (err) {
     if (err) {
         console.log(err);
         return;
